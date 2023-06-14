7ea9fc17299bc9fa6dba9200a5892ec1f374e564
tiagofraga342 <tiago.fraga342@gmail.com>
2023-06-14 18:28:49 -0300

    Implementa a classe Player

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
 	public static void main(String[] args) {
-		Card.createCards();
+		Card[] gameCards = Card.createCards();
+		Player tiago = new Player("Tiago", Color.RED, gameCards[0], gameCards[1]);
+		System.out.println("Old tiago cards: " + tiago.getCards()[0].getName() + " " + tiago.getCards()[1].getName());
+		tiago.swapCard(tiago.getCards()[0], gameCards[2]);
+		System.out.println("New tiago cards: " + tiago.getCards()[0].getName() + " " + tiago.getCards()[1].getName());
 	}
 }
--- Player.java
+++ Player.java
@@ -2,6 +2,9 @@
  * Classe que contém informações e ações básicas relacionadas aos jogadores
  */
 public class Player {
+	private final String name;
+	private final Color pieceColor;
+	private Card[] cards = new Card[2];
     /**
      * Construtor que define informações básicas do jogador
      * @param name Nome do jogador
@@ -9,7 +12,9 @@ public class Player {
      * @param cards Cartas na mão do jogador
      */
     public Player(String name, Color pieceColor, Card[] cards) {
-
+    	this.name = name;
+    	this.pieceColor = pieceColor;
+    	this.cards = cards;
     }
 
     /**
@@ -20,7 +25,10 @@ public class Player {
      * @param card2 A segunda carta na mão do jogador
      */
     public Player(String name, Color pieceColor, Card card1, Card card2) {
-
+    	this.name = name;
+		this.pieceColor = pieceColor;
+		cards[0] = card1;
+		cards[1] = card2;
     }
 
     /**
@@ -28,7 +36,7 @@ public class Player {
      * @return String com o nome do jogador(a)
      */
     public String getName() {
-        return null;
+        return name;
     }
 
     /**
@@ -36,7 +44,7 @@ public class Player {
      * @return Enum Color com a cor das peças do jogador
      */
     public Color getPieceColor() {
-        return null;
+        return pieceColor;
     }
 
     /**
@@ -44,7 +52,7 @@ public class Player {
      * @return Booleano true para caso seja um mestre e false caso contrário
      */
     public Card[] getCards() {
-        return null;
+        return cards;
     }
 
     /**
@@ -54,6 +62,23 @@ public class Player {
      * @exception InvalidCardException Caso a carta não esteja na mão do jogador e/ou na mesa
      */
     protected void swapCard(Card oldCard, Card newCard) throws InvalidCardException {
-
+    	//TODO: Verifica se a carta está na mão do jogador ou na mesa
+    	int i;
+    	boolean isOnHand = false;
+    	for(i = 0; i < 2; i++) {
+    		if(cards[i] == oldCard) {
+    			isOnHand = true;
+    			break;
+    		}
+    	}
+    	
+    	// Como acessar o deck da partida atual?
+    	// if(cards[4] == newCard) throw new InvalidCardException("Erro na tentativa de troca das cartas: a nova carta não está na mesa");
+    	// if(!isOnHand) throw new InvalidCardException("Erro na tentativa de troca das cartas: a carta antiga não esta na mão do jogador");
+    	
+    	Card temp = newCard;
+    	cards[i] = newCard;
+		newCard = oldCard;
+		oldCard = temp;
     }
 }
