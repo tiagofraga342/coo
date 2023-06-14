c8f5c3927fcab1856895d41066a771acd01959c5
tiagofraga342 <tiago.fraga342@gmail.com>
2023-06-14 15:34:36 -0300

    Implementa classe Piece

--- Piece.java
+++ Piece.java
@@ -2,13 +2,16 @@
  * Classe que contém informações das peças de jogo
  */
 public class Piece {
+	private Color color;
+	private boolean isMaster;
     /**
      * Construtor que define a cor e o tipo da peça
      * @param color Cor da peça
      * @param isMaster Se o tipo da peça é mestre ou não
      */
     public Piece(Color color, boolean isMaster) {
-
+    	this.color = color;
+    	this.isMaster = isMaster;
     }
 
     /**
@@ -16,7 +19,7 @@ public class Piece {
      * @return Enum Color com a cor da peça
      */
     public Color getColor() {
-        return null;
+        return color;
     }
 
     /**
@@ -24,6 +27,6 @@ public class Piece {
      * @return Booleano true para caso seja um mestre e false caso contrário
      */
     public boolean isMaster() {
-        return false;
+        return isMaster;
     }
 }
