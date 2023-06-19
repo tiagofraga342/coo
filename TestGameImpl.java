c89b36a938f71318bb640db42f84033a7570ed52
Tiago <72491366+tiagofraga342@users.noreply.github.com>
2023-06-19 00:25:07 +0000

    Revisa o código dos testes

--- TestGameImpl.java
+++ TestGameImpl.java
@@ -5,23 +5,7 @@ import static org.junit.Assert.*;
 public class TestGameImpl {
 	GameImpl g1;
 	GameImpl g2;
-	GameImpl g3;
-	/*GameImpl jogo1 = new GameImpl("Tiago", "sla");
-		jogo1.printBoard();
-		System.out.println(jogo1.getBluePlayer().getName());
-		System.out.println(jogo1.getRedPlayer().getName());
-		System.out.println("Current player: " + jogo1.getCurrentPlayer().getName());
-		System.out.println("Table card: " + jogo1.getTableCard().getName());
-		Card[] tiagoCards = jogo1.getRedPlayer().getCards();
-		System.out.println("Cartas tiago:");
-		for(int i = 0; i < 2; i++) {
-			System.out.println(tiagoCards[i].getName());
-		}
-		jogo1.makeMove(jogo1.getCurrentPlayer().getCards()[1], jogo1.getCurrentPlayer().getCards()[1].getPositions()[0], new Position(0, 2));
-		jogo1.printBoard();
-	*/
 
-	// Setup
 	@Before
 	public void setupGame() {
 		g1 = new GameImpl();
@@ -30,12 +14,8 @@ public class TestGameImpl {
 		g2 = new GameImpl("Tiago Vermelho", "Tiago Azul");
 		if(g2 == null) 
 			fail("Falha na criação do objeto!");
-		//g3 = new GameImpl("Tiago Vermelho", "Tiago Azul"); // testar com deck aleatório
-		//if(g3 == null) 
-		//	fail("Falha na criação do objeto!");
 	}
 
-	// Teste dos construtores
 	@Test
 	public void testBuildBoard() {
 
@@ -63,30 +43,23 @@ public class TestGameImpl {
 
 	@Test
 	public void testGetPlayers() {
-		// Para g1
 		assertEquals("Jogador Azul", g1.getBluePlayer().getName());
 		assertEquals("Jogador Vermelho", g1.getRedPlayer().getName());
 		assertEquals(Color.BLUE, g1.getBluePlayer().getPieceColor());
 		assertEquals(Color.RED, g1.getRedPlayer().getPieceColor());
-		// pega a table card
-		// confere qual jogador deveria ser o primeiro e se ele é o currentPlayer
+
 		Card t1 = g1.getTableCard();
 		assertEquals(t1.getColor(), g1.getCurrentPlayer().getPieceColor());
 	
-		// Para g2
 		assertEquals("Tiago Azul", g2.getBluePlayer().getName());
 		assertEquals("Tiago Vermelho", g2.getRedPlayer().getName());
 		assertEquals(Color.BLUE, g2.getBluePlayer().getPieceColor());
 		assertEquals(Color.RED, g2.getRedPlayer().getPieceColor());
-		// pega a table card
-		// confere qual jogador deveria ser o primeiro e se ele é o currentPlayer
+
 		Card t2 = g2.getTableCard();
 		assertEquals(t2.getColor(), g2.getCurrentPlayer().getPieceColor());
 	}
 
-	// Teste do makeMove
-
-	// Teste que tenta fazer um movimento para fora do tabuleiro
 	@Test(expected = IllegalMovementException.class)
 	public void testMakeMoveOutsideTheBoard() {
 		Position[] p1 = { new Position(5, 5) };
@@ -97,7 +70,6 @@ public class TestGameImpl {
 			g1.makeMove(c1, p1[0], new Position(0, 0));
 	}
 
-	// Teste que tenta fazer um movimento sendo o player azul no turno do player vermelho
 	@Test(expected = IncorrectTurnOrderException.class)
 	public void testMakeMoveIncorrectTurn() {
 		Color currentPlayerColor = g1.getCurrentPlayer().getPieceColor();
@@ -116,7 +88,6 @@ public class TestGameImpl {
 		}
 	}	
 
-	// Teste que tenta fazer um movimento que incide numa peça da mesma cor
 	@Test(expected = IllegalMovementException.class)
 	public void testMakeMoveSameColorPiece() {
 		Position[] p1 = { new Position(0, 1) };
@@ -128,7 +99,6 @@ public class TestGameImpl {
 		
 	}
 
-	// Teste que tenta usar uma peça que não exista
 	@Test(expected = InvalidPieceException.class)
 	public void testeMakeMoveInvalidPiece() {
 		Position[] p1 = { new Position(0, 1) };
@@ -136,7 +106,6 @@ public class TestGameImpl {
 		g1.makeMove(c1, p1[0], new Position(2, 2));
 	}
 
-	// Teste que tenta usar uma carta que não esteja na mão do jogador
 	@Test(expected = InvalidCardException.class)
 	public void testMakeMoveWrongCard() {
 		Position[] p1 = { new Position(1, 1) };
@@ -148,9 +117,6 @@ public class TestGameImpl {
 			g1.makeMove(c1, p1[0], new Position(0, 1));
 	}
 
-	// Teste que verifica se a carta da mesa foi trocada com a carta da mão
-
-	// Teste se o currentPlayer foi alterado após um movimento
 	@Test
 	public void testMakeMoveChangeCurrentPlayer() {
 		Position[] p1 = { new Position(1, 1) };
@@ -166,30 +132,25 @@ public class TestGameImpl {
 		}
 	}
 
-	// Teste do checkVictory
 	@Test
 	public void testCheckVictoryByDefeat() {
-		// Retira o mestre azul
 		g1.getBoard()[0][2].releaseSpot();
 		assertTrue(g1.checkVictory(Color.RED));
 
-		// Retira o mestre vermelho
 		g1.getBoard()[4][2].releaseSpot();
 		assertTrue(g1.checkVictory(Color.BLUE));
 	}
 
 	@Test
-	public void testCheckVictoryByTemple() {
+	public void testCheckVictoryByTempleTakeOver() {
 		Position[] p1 = { new Position(1, 1) };
 		g1.setCurrentPlayer(Color.BLUE);
 		Card c1 = g1.getCurrentPlayer().getCards()[0];
 		
-		// Move o mestre azul para fora do templo
 		g1.makeMove(c1, p1[0], new Position(0, 2));
 
 		Position[] p2 = { new Position(-4, 0) };
 		Card c2 = g1.getCurrentPlayer().getCards()[0];
-		// Move o mestre vermelho para dentro do templo azul
 		g1.makeMove(c2, p2[0], new Position(4, 2));
 
 		assertTrue(g1.checkVictory(Color.RED));
--- TestSpot.java
+++ TestSpot.java
@@ -8,7 +8,6 @@ public class TestSpot {
 
 	@Before
 	public void buildBoardSetup() {
-		// Meio do tabuleiro
 		for(int i = 1; i < BOARD_SIZE - 1; i++) {
 			for(int j = 0; j < BOARD_SIZE; j++) {
 				board[i][j] = new Spot(new Position(i, j));
@@ -17,9 +16,7 @@ public class TestSpot {
 					fail("Falha na criação do objeto!");
 			}
 		}
-		// Parte de colorida
 		for(int i = 0; i < BOARD_SIZE; i++) {
-			// Templo
 			if(i == 2){
 				board[0][i] = new Spot(new Piece(Color.BLUE, true), new Position(0, i), Color.BLUE);
 				board[4][i] = new Spot(new Piece(Color.RED, true), new Position(4, i), Color.RED);
@@ -34,14 +31,7 @@ public class TestSpot {
 		}
 	}
 
-	// Teste dos métodos getters
 	@Test
-	/*public void testGetPiece() {
-		for(int i = 0; i < BOARD_SIZE; i++) {
-			assertTrue(board[4][i].getPiece().getColor().equals(Color.RED));
-			assertTrue(board[0][i].getPiece().getColor().equals(Color.BLUE));
-		}
-	}*/
 	public void testGetPiece() {
 		for(int i = 0; i < BOARD_SIZE; i++) {
 			assertEquals(board[0][i].getPiece().getColor(), Color.BLUE);
@@ -60,30 +50,25 @@ public class TestSpot {
 
 	@Test
 	public void testTempleSpot() {
-		// Teste se o templo está 
-		// na posição correta e com a cor correta e com o mestre
 		assertTrue(board[0][2].getColor().equals(Color.BLUE) && board[0][2].getPiece().isMaster());
 		assertTrue(board[4][2].getColor().equals(Color.RED) && board[4][2].getPiece().isMaster());
  	}
 	
-	// Teste do occupySpot
  	@Test
  	public void testOccupySpot() {
- 		// Tenta ocupar um Spot válido
  		Piece p = board[4][0].getPiece();
  		board[2][2].occupySpot(board[4][0].getPiece());
  		
  		assertEquals(p, board[2][2].getPiece()); 
  	}
+
  	@Test(expected = IllegalMovementException.class)
  	public void testOccupySpotException() {
  		board[0][1].occupySpot(board[0][0].getPiece());
  	}
 
-	// Teste do releaseSpot
 	@Test
 	public void testReleaseSpot() {
-		// Verifica se o Spot liberado realmente foi liberado
 		board[1][1].occupySpot(board[0][0].getPiece());
 		board[1][1].releaseSpot();
 		assertNull(board[1][1].getPiece());
